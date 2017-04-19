0bf4eba65db50c2602071f16ccb1b385064324bf
Justin <justintt1995@gmail.com>
2017-04-19 00:37:11 -0400

    toy dataset script with train test split

--- data/get_inria3D.py
+++ data/get_inria3D.py
@@ -9,17 +9,18 @@ import numpy as np
 import os, tarfile
 import h5py
 
-def scale_img(path, dims = (180, 320)):
+def resize_scale_img(path, dims = (180, 320), scale = False):
 	img = skimage.io.imread(path)
 
 	# Scale down color channels
-	img = img / 255.0
-	assert (0 <= img).all() and (img <= 1.0).all()
+	if scale:
+		img = img / 255.0
+		assert (0 <= img).all() and (img <= 1.0).all()
+
 	# Resize
 	resized_img = skimage.transform.resize(img, dims, mode = "reflect")
 
 	return resized_img
-
 
 def download_inria():
 	import urllib2
@@ -72,10 +73,6 @@ def extract_tar():
 			os.remove(file_name) 
 			print "Untarred: " + item
 
-
-def load_inria_frame(dims = (180,320)):
-	print "Collecting Inria Dataset Frames"
-
 	#Remove Viz Folders with only generic jpg frames
 	print "Removing Viz Folders"
 	dir_name = os.getcwd()
@@ -87,58 +84,88 @@ def load_inria_frame(dims = (180,320)):
 	except:
 		pass
 
+
+def load_inria_frame(dims = (180,320), terminate = None, prob = 0.2):
+	print "Collecting Inria Dataset Frames"
+
 	# Recursively walk down directorie and collect Frames and concat to X and Y
-	dir_name = os.getcwd()
+	cur_dir = os.getcwd()
 	l_ext = ".jpg"
 	r_ext = ".right"
+	out_ext = ".jpeg"
 
-	dimensions = (0, dims[0], dims[1], 3)
-	X = np.zeros(dimensions)
-	Y = np.zeros(dimensions)
+	# Add directories
+	if not os.path.exists(os.path.join(cur_dir, "frames")):
+		print "Creating Frames Folder... "
+		os.makedirs(os.path.join(cur_dir, "frames", "train", "left"))
+		os.makedirs(os.path.join(cur_dir, "frames", "train", "right"))
+		os.makedirs(os.path.join(cur_dir, "frames", "test", "left"))
+		os.makedirs(os.path.join(cur_dir, "frames", "test", "right"))
 
 	# Calculate Total Frames
 	count = 0 
-	ds_count = 0
 	tot_count = 0 
 
-	for root, dirs, files in os.walk(dir_name, topdown=False):
+	for root, dirs, files in os.walk(cur_dir, topdown=False):
 		for file in files:
 			if file.endswith(l_ext): 
 				tot_count = tot_count + 2
 
-	print "Collecting " + str(tot_count) +  " frames and downsampling them."
+
+	terminate_count = tot_count
+	if terminate is not None:
+		if terminate * 2 > tot_count:
+			print "Error: Requested number of pairs larger than total number of pairs avail"
+			return
+		terminate_count = terminate * 2
+
+
+
+	print "Collecting " + str(terminate_count) +  " frames and downsampling them."
 	print "This will take awhile, be patient =D."
 
+
+	# Create seeded mask for determining Test/Train Split
+	np.random.seed(100)
+	test_mask = np.random.choice([0,1], size =(terminate_count/2,), p = [1-prob, prob])
+
+
 	# The actual walk
-	for root, dirs, files in os.walk(dir_name, topdown = True):
+	for root, dirs, files in os.walk(cur_dir, topdown = True):
 		for file in files:
 			if file.endswith(l_ext): 
 				path = os.path.join(root, file)
-				l_frame = scale_img(path, dims).reshape((1, dims[0], dims[1], 3))
 
 				try:
-					r_frame = scale_img(path+r_ext, dims).reshape((1, dims[0], dims[1], 3))
-					X = np.concatenate((X, l_frame), axis = 0)
-					Y = np.concatenate((Y, r_frame), axis = 0)
+					l_frame = resize_scale_img(path, dims)
+					r_frame = resize_scale_img(path+r_ext, dims)
+					
+					# Write back to disk in designated Folder 
+					if test_mask[count/2] == 0:
+						folder = "train"
+					else:
+						folder = "test"
+						
+					l_path = os.path.join(cur_dir, "frames", folder, "left", str(count/2)+out_ext)
+					r_path = os.path.join(cur_dir, "frames", folder, "right", str(count/2)+out_ext)
+					skimage.io.imsave(l_path, l_frame)
+					skimage.io.imsave(r_path, r_frame)
 					count = count + 2
 				except IOError:	
-					print "Missing Right Frame, Ignoring Entry"
+					print "IO Error (Missing Right Frame/Mem Limit/etc) , Ignoring Entry"
 
 				if count % 250 == 0 and count != 0:
-					print  "(" + str(count) + "/" + str(tot_count) + ")"
+					print  "(" + str(count) + "/" + str(terminate_count) + ")"
 
-				if count % 1000 == 0 and count != 0:
-					write_to_disk(X,Y, "inria_data.h5", str(ds_count))
-					ds_count = ds_count + 1
-					del X
-					del Y
-					X = np.zeros(dimensions)
-					Y = np.zeros(dimensions)
+				if count == terminate_count:
+					break 
+		if count == terminate_count:
+			break 
 
-	write_to_disk(X,Y, "inria_data.h5", str(ds_count))
 	return
 
 
+# H5 Writing to Disk Function
 def write_to_disk(X, Y, f_name, ds_ext):
 	print "Writing " + "X_/Y_" + ds_ext + " to " + f_name
 	h5f = h5py.File(f_name, 'a')
@@ -147,14 +174,11 @@ def write_to_disk(X, Y, f_name, ds_ext):
 	h5f.close()
 	print "Write Complete"
 
+# -------------------------------------------------------------- #
 
 def main():
 	import time
 	start = time.time()
-
-	if os.path.exists('inria_data.h5'):
-		print "Some inria data already stored in h5 format"
-		return
 
 	if os.path.exists('inria_stereo_dataset/'):
 		print "Inria data already downloaded. Using cached files"
@@ -166,6 +190,8 @@ def main():
 
 	end = time.time()
 	print "Extraction Completed... Took " + str((end - start)/60) + " minutes"
+
+
 
 if __name__ == "__main__":
 	main()
